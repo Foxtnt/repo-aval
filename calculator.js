9fdd950862c4c25fab0b1dba0e45b0feec102af3
Foxtnt <bruno_correa_da_silva@yahoo.com.br>
2026-04-07 22:10:14 -0300

    :sparkles:Adicionando comando de divisão

--- calculator.js
+++ calculator.js
@@ -13,7 +13,13 @@ function sum(v1, v2) {
 //function sub(v1, v2) {}
 
 // retorna o valor da divisão entre os dois parâmetros
-//function div(v1, v2) {}
+function div(v1, v2) {
+    if (v2 === 0) {
+        return 'Erro: divisão por zero'
+    } else {
+        return v1 / v2
+    }
+}
 
 // retorna o valor da multiplicação entre os dois parâmetros
 //function mult(v1, v2) {}
@@ -22,3 +28,4 @@ function sum(v1, v2) {
 //function square(v1) {}
 
 console.log('Soma:', sum(valorA, valorB))
+console.log('A divisão resulta em: ', div(valorA, valorB))
